d783288f6029450867e063db8d8a8473104b0fff
Katie Hoffman <ktahoffman@gmail.com>
2014-07-14 11:53:08 -0400

    fix path

--- spec/spotify_chart_spec.rb
+++ spec/spotify_chart_spec.rb
@@ -3,13 +3,20 @@ require_relative 'spec_helper'
 describe SpotifyChart do
 
   let(:american_chart) {SpotifyChart.new("us")}
+  let(:british_chart) {SpotifyChart.new("gb")}
 
   describe '#most_streamed' do
-    it "returns the track title, artist, and album for a given country's top streamed song" do
+    it "returns America's most streamed track title, artist, and album" do
       expect(american_chart.most_streamed).to eq(SpecSpotifyChart.new("us").most_streamed)
     end
-    it "returns the track title, artist, and album for a given country's top shared song" do
+    it "returns America's most shared track title, artist, and album" do
       expect(american_chart.most_shared).to eq(SpecSpotifyChart.new("us").most_shared)
+    end
+    it "returns Great Britain's most streamed track title, artist, and album" do
+      expect(british_chart.most_streamed).to eq(SpecSpotifyChart.new("gb").most_streamed)
+    end
+    it "returns Great Britain's most shared track title, artist, and album" do
+      expect(british_chart.most_shared).to eq(SpecSpotifyChart.new("gb").most_shared)
     end
   end
 
