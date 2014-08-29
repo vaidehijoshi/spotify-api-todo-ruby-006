3f13e89f5805b09eb5227bc541926e85f9a3aef4
Katie Hoffman <ktahoffman@gmail.com>
2014-08-29 15:21:36 -0400

    quick tidying up to trigger curriculum

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,29 +8,6 @@ require_relative '../spotify_chart.rb'
 require_relative './spec_spotify_chart.rb'
 
 RSpec.configure do |config|
-  # Use color not only in STDOUT but also in pagers and files
   config.tty = true
-
-  # A '.' represents a passing example, 'F' is failing, and '*' is pending.
-  config.formatter = :progress
-
-  # Use the treat_symbols_as_metadata_keys_with_true_values option to tell RSpec that :key is shorthand for :key => true.
-  # config.treat_symbols_as_metadata_keys_with_true_values = true
-
-  # Limit the spec run to only specs with the focus metadata. If no specs have
-  # the filtering metadata and `run_all_when_everything_filtered = true` then
-  # all specs will run.
-  # config.filter_run :focus
-
-  # Run all specs when none match the provided filter. This works well in
-  # conjunction with `config.filter_run :focus`, as it will run the entire
-  # suite when no specs have `:filter` metadata.
-  # config.run_all_when_everything_filtered = true
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  # config.order = 'random'
   config.order = 'default'
-  
 end
