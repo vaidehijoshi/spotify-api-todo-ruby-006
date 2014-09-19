03d1a360be4b8def6036ac3b3d6836bf3e0bb324
Katie Hoffman <ktahoffman@gmail.com>
2014-09-19 14:03:49 -0400

    refactor

--- spec/spotify_chart_spec.rb
+++ spec/spotify_chart_spec.rb
@@ -1,45 +1,28 @@
 describe SpotifyChart do
-
-  before(:all) do
-    encoded = "cmVxdWlyZSAnb3Blbi11cmknCnJlcXVpcmUgJ2pzb24nCgpjbGFzcyBTcGVj\nU3BvdGlmeUNoYXJ0CiAgYXR0cl9yZWFkZXIgOnN0cmVhbWVkLCA6c2hhcmVk\nCgogIGRlZiBpbml0aWFsaXplKHJlZ2lvbl9hYmJyZXZpYXRpb24pCiAgICBi\nYXNlX3VybCA9ICJodHRwOi8vY2hhcnRzLnNwb3RpZnkuY29tL2FwaS9jaGFy\ndHMiCiAgICBAc3RyZWFtZWQgPSBKU09OLmxvYWQob3BlbigiI3tiYXNlX3Vy\nbH0vbW9zdF9zdHJlYW1lZC8je3JlZ2lvbl9hYmJyZXZpYXRpb259L2xhdGVz\ndCIpKQogICAgQHNoYXJlZCA9IEpTT04ubG9hZChvcGVuKCIje2Jhc2VfdXJs\nfS9tb3N0X3NoYXJlZC8je3JlZ2lvbl9hYmJyZXZpYXRpb259L2xhdGVzdCIp\nKQogIGVuZAoKICBkZWYgbW9zdF9zdHJlYW1lZAogICAgYXJ0aXN0ID0gc3Ry\nZWFtZWRbInRyYWNrcyJdWzBdWyJhcnRpc3RfbmFtZSJdCiAgICB0cmFja190\naXRsZSA9IHN0cmVhbWVkWyJ0cmFja3MiXVswXVsidHJhY2tfbmFtZSJdIAog\nICAgYWxidW0gPSBzdHJlYW1lZFsidHJhY2tzIl1bMF1bImFsYnVtX25hbWUi\nXSAKICAgICInI3t0cmFja190aXRsZX0nIGJ5ICcje2FydGlzdH0nIGZyb20g\ndGhlIGFsYnVtICcje2FsYnVtfSciCiAgZW5kCgogIGRlZiBtb3N0X3NoYXJl\nZAogICAgYXJ0aXN0ID0gc2hhcmVkWyJ0cmFja3MiXVswXVsiYXJ0aXN0X25h\nbWUiXQogICAgdHJhY2tfdGl0bGUgPSBzaGFyZWRbInRyYWNrcyJdWzBdWyJ0\ncmFja19uYW1lIl0gCiAgICBhbGJ1bSA9IHNoYXJlZFsidHJhY2tzIl1bMF1b\nImFsYnVtX25hbWUiXSAKICAgICInI3t0cmFja190aXRsZX0nIGJ5ICcje2Fy\ndGlzdH0nIGZyb20gdGhlIGFsYnVtICcje2FsYnVtfSciCiAgZW5kCgplbmQ=\n"
-    decoded = Base64.decode64(encoded)
-    File.open('./spec/spec_spotify_chart.rb', 'w') do |file| 
-      file.write(decoded)
-    end
-    require './spec/spec_spotify_chart.rb'
-  end
-
-  after(:all) do
-    File.open('./spec/spec_spotify_chart.rb', 'w') do |file| 
-      file.truncate(0)
-    end
-  end
-
-  let(:american_chart) {SpotifyChart.new("us")}
-  let(:british_chart) {SpotifyChart.new("gb")}
+  # test spec setup
+  let(:test_us_chart) { SpecSpotifyChart.new("us") }
+  let(:test_gb_chart) { SpecSpotifyChart.new("gb") }
+  # your file spec setup
+  let(:spotify_chart) { SpotifyChart.new }
 
   describe '#most_streamed' do
-
     it "returns America's most streamed track title, artist, and album" do
-      expect(american_chart.most_streamed).to eq(SpecSpotifyChart.new("us").most_streamed)
+      expect(spotify_chart.most_streamed("us")).to eq(test_us_chart.most_streamed)
     end
 
     it "returns Great Britain's most streamed track title, artist, and album" do
-      expect(british_chart.most_streamed).to eq(SpecSpotifyChart.new("gb").most_streamed)
+      expect(spotify_chart.most_streamed("gb")).to eq(test_gb_chart.most_streamed)
     end
-
   end
 
   describe '#most_shared' do
-    
     it "returns America's most shared track title, artist, and album" do
-      expect(american_chart.most_shared).to eq(SpecSpotifyChart.new("us").most_shared)
+      expect(spotify_chart.most_shared("us")).to eq(test_us_chart.most_shared)
     end
 
     it "returns Great Britain's most shared track title, artist, and album" do
-      expect(british_chart.most_shared).to eq(SpecSpotifyChart.new("gb").most_shared)
+      expect(spotify_chart.most_shared("gb")).to eq(test_gb_chart.most_shared)
     end
-
   end
 
 end
--- spotify_chart.rb
+++ spotify_chart.rb
@@ -2,25 +2,38 @@ require 'open-uri'
 require 'json'
 
 class SpotifyChart
-  attr_reader :streamed, :shared
+  attr_reader :base_url
+  attr_accessor :region, :version, :url
 
-  def initialize(region_abbreviation)
-    base_url = "http://charts.spotify.com/api/charts"
-    @streamed = JSON.load(open("#{base_url}/most_streamed/#{region_abbreviation}/latest"))
-    @shared = JSON.load(open("#{base_url}/most_shared/#{region_abbreviation}/latest"))
+  def initialize
+    @base_url = "http://charts.spotify.com/api/charts/"
+    @region = ""
+    @version = ""
   end
 
-  def fetch_track_album_artist(json)
-    song = json["tracks"][0]
+  def get_url
+    base_url + version + "/" + region  + "/latest"
+  end
+
+  def get_json
+    JSON.load(open(get_url))
+  end
+
+  def fetch_track_album_artist
+    song = get_json["tracks"][0]
     "'#{song["track_name"]}' by '#{song["artist_name"]}' from the album '#{song["album_name"]}'"
   end
 
-  def most_streamed
-    fetch_track_album_artist(streamed)
+  def most_streamed(region)
+    self.region = region
+    self.version = "most_streamed"
+    fetch_track_album_artist
   end
 
-  def most_shared
-    fetch_track_album_artist(shared)
+  def most_shared(region)
+    self.region = region
+    self.version = "most_shared"
+    fetch_track_album_artist
   end
 
 end
