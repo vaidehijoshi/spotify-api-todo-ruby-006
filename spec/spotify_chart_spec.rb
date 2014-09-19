118845e324ed5fdf07bba7d990cfbaf6a180ea5d
Katie Hoffman <ktahoffman@gmail.com>
2014-09-19 14:36:26 -0400

    passes all tests

--- spec/spotify_chart_spec.rb
+++ spec/spotify_chart_spec.rb
@@ -1,11 +1,78 @@
 describe SpotifyChart do
+
+  describe '#initialize' do
+    it "does not raise error when called on with no arguments" do
+      expect { SpotifyChart.new }.to_not raise_error
+    end
+
+    it "sets a variable 'base_url' as the root url of the Spotify Chart API" do
+      expect(SpotifyChart.new.base_url).to eq("http://charts.spotify.com/api/charts/")
+    end
+  end
+
+  let(:spotify_chart) { SpotifyChart.new }
+
+  describe "#get_url" do
+    it "accepts two arguments, most_streamed/most_shared and region" do
+      expect { spotify_chart.get_url("most_streamed", "us") }.to_not raise_error
+    end
+
+    it "returns a string" do
+      expect(spotify_chart.get_url("most_streamed", "us").class).to eq(String)
+    end
+
+    it "returns a string with the base url followed by 'most_streamed',
+    including the region abbreviation,
+    and ending with 'latest' when most streamed is queried" do
+      region = "gb"
+      my_url = spotify_chart.get_url("most_streamed", "gb")
+      regex = /http:\/\/charts.spotify.com\/api\/charts\/most_streamed\//
+      regex_results = [regex.match(my_url), /\/latest/.match(my_url), /#{region}/.match(my_url)]
+      regex_results.each do |match|
+        expect(match).to_not be_nil
+      end
+    end
+
+    it "returns a string with the base url followed by 'most_shared', 
+    including the region abbreviation, 
+    and ending with 'latest' when most shared is queried" do
+      region = "us"
+      my_url = spotify_chart.get_url("most_shared", region)
+      regex = /http:\/\/charts.spotify.com\/api\/charts\/most_shared\//
+      regex_results = [regex.match(my_url), /\/latest/.match(my_url), /#{region}/.match(my_url)]
+      regex_results.each do |match|
+        expect(match).to_not be_nil
+      end
+    end
+
+    it "returns the correct url for querying the API based on most shared/streamed and region abbreviation" do
+      expect(spotify_chart.get_url("most_streamed", "gb")).to eq("http://charts.spotify.com/api/charts/most_streamed/gb/latest")
+      expect(spotify_chart.get_url("most_shared", "us")).to eq("http://charts.spotify.com/api/charts/most_shared/us/latest")
+    end
+  
+  end
+
+  describe "#get_json" do
+    let(:url) { "http://api.openweathermap.org/data/2.5/weather?q=NewYork" }
+    
+    it "accepts one argument, a JSON url" do
+      expect { spotify_chart.get_json(url) }.to_not raise_error
+    end
+
+    it "returns a hash" do
+      expect(spotify_chart.get_json(url).class).to eq(Hash)
+    end
+
+  end
   # test spec setup
   let(:test_us_chart) { SpecSpotifyChart.new("us") }
   let(:test_gb_chart) { SpecSpotifyChart.new("gb") }
-  # your file spec setup
-  let(:spotify_chart) { SpotifyChart.new }
 
   describe '#most_streamed' do
+    it "accepts one argument, the region" do
+      expect { spotify_chart.most_streamed("us") }.to_not raise_error
+    end
+
     it "returns America's most streamed track title, artist, and album" do
       expect(spotify_chart.most_streamed("us")).to eq(test_us_chart.most_streamed)
     end
@@ -16,6 +83,10 @@ describe SpotifyChart do
   end
 
   describe '#most_shared' do
+    it "accepts one argument, the region" do
+      expect { spotify_chart.most_shared("us") }.to_not raise_error
+    end
+
     it "returns America's most shared track title, artist, and album" do
       expect(spotify_chart.most_shared("us")).to eq(test_us_chart.most_shared)
     end
--- spotify_chart.rb
+++ spotify_chart.rb
@@ -3,37 +3,32 @@ require 'json'
 
 class SpotifyChart
   attr_reader :base_url
-  attr_accessor :region, :version, :url
 
   def initialize
     @base_url = "http://charts.spotify.com/api/charts/"
-    @region = ""
-    @version = ""
   end
 
-  def get_url
+  def get_url(version, region)
     base_url + version + "/" + region  + "/latest"
   end
 
-  def get_json
-    JSON.load(open(get_url))
+  def get_json(url)
+    JSON.load(open(url))
   end
 
-  def fetch_track_album_artist
-    song = get_json["tracks"][0]
+  def fetch_track_album_artist(json)
+    song = json["tracks"][0]
     "'#{song["track_name"]}' by '#{song["artist_name"]}' from the album '#{song["album_name"]}'"
   end
 
   def most_streamed(region)
-    self.region = region
-    self.version = "most_streamed"
-    fetch_track_album_artist
+    json = get_json(get_url("most_streamed", region))
+    fetch_track_album_artist(json)
   end
 
   def most_shared(region)
-    self.region = region
-    self.version = "most_shared"
-    fetch_track_album_artist
+    json = get_json(get_url("most_shared", region))
+    fetch_track_album_artist(json)
   end
 
 end
