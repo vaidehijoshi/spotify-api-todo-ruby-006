c1bd88cd488d3222703f3ffa933127a3c2b7a883
vaidehijoshi <vaidehi.sj@gmail.com>
2014-10-08 13:57:53 -0400

    made changes

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -6,19 +6,25 @@ class SpotifyChart
   attr_reader :base_url
 
   def initialize
-    @base_url = "?"
+    @base_url = "http://charts.spotify.com/api/charts/"
   end
 
   def get_url(preference, region)
-    # return a string that is the base url + / + preference + / + region + / + latest
+    "#{@base_url}#{preference}/#{region}/latest"
   end
 
   def get_json(url)
+    JSON.load(open(url))
+
     # load json given a url here
     # refer to the references if you have questions about this
   end
 
   def fetch_track_album_artist(music_hash)
+    track_name = music_hash['tracks'][0]["track_name"]
+    artist_name = music_hash['tracks'][0]["artist_name"]
+    album_name = music_hash['tracks'][0]["album_name"]
+    "'#{track_name}' by #{artist_name} from the album #{album_name}"
     # example music_hash:
     # {  
     #   "tracks" => [  
@@ -50,7 +56,7 @@ class SpotifyChart
     # and set it equal to a variable
 
     # call on get_json here, using the string that get_url returns
-    
+    fetch_track_album_artist(get_json)
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
   end
